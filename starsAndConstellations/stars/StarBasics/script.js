8e90cf8966440bf5d855ef24a4e5f706ad3c96bd
tsvetinkv <cnikolova95@gmail.com>
2023-11-26 21:20:24 +0200

    clear input field after value is send

--- starsAndConstellations/stars/StarBasics/script.js
+++ starsAndConstellations/stars/StarBasics/script.js
@@ -1,13 +1,26 @@
+// Set the flag and redirect when 'Enter' key is pressed
 document.querySelector('#input').addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
         var value = e.target.value;
         window.location.href = '/starsAndConstellations/stars/StarSearch/index.html?value=' + encodeURIComponent(value);
+        localStorage.setItem('clearInput', 'true');
     }
 });
 
+// Set the flag and redirect when input field is clicked
 document.querySelector('#input').addEventListener('click', function (e) {
     let value = e.target.value;
     if (value) {
         window.location.href = '/starsAndConstellations/stars/StarSearch/index.html?value=' + encodeURIComponent(value);
+        localStorage.setItem('clearInput', 'true');
     }
 });
+
+// Clear the input field when the page loads, if the flag is set
+window.onload = function() {
+    if (localStorage.getItem('clearInput') === 'true') {
+        document.querySelector('#input').value = '';
+        localStorage.removeItem('clearInput');
+    }
+};
+
