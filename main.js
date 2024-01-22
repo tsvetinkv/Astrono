6e23e291e63aa4782d39476281fbbf18e5a2e96c
tsvetinkv <cnikolova95@gmail.com>
2024-01-22 09:44:20 +0200

    update three.js imports

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'OrbitControls';
+import * as THREE from "https://cdn.skypack.dev/three@0.132.2";
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/OrbitControls.js";
 
 
 // Parameters
