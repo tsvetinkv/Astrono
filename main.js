6ca68c6e182a48496a93f6071859209728102109
tsvetinkv <cnikolova95@gmail.com>
2024-01-22 12:06:14 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-// import * as THREE from "https://cdn.skypack.dev/three@0.132.2";
-// import { OrbitControls } from "https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/OrbitControls.js";
+import * as THREE from "https://cdn.skypack.dev/three@0.132.2";
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/OrbitControls.js";
 
-import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+// import * as THREE from "three";
+// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 // Parameters
 const parameters = {};
