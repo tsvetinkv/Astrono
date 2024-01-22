cc5d57d996ea41f12bf1a31cb70df562affbd9a1
tsvetinkv <cnikolova95@gmail.com>
2024-01-22 12:06:57 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+import "./style.css";
 import * as THREE from "https://cdn.skypack.dev/three@0.132.2";
 import { OrbitControls } from "https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/OrbitControls.js";
+
 
 // import * as THREE from "three";
 // import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
