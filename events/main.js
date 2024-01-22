a51b9cd16c6ae47d1731ee00c16cb1c360b9514d
tsvetinkv <cnikolova95@gmail.com>
2024-01-22 10:17:06 +0200

    changed imports

--- events/main.js
+++ events/main.js
@@ -1,5 +1,6 @@
-import * as THREE from "./node_modules/three";
-import { OrbitControls } from "./node_modules/three/examples/jsm/controls/OrbitControls";
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const scene = new THREE.Scene();
 
